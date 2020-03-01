6abdfdc9d1cf47fc848bdd5b5874249d8d546de2
Calvin Gozé <49960740+calvingoze@users.noreply.github.com>
2020-03-01 16:50:28 -0600

    Added Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './main/home/home.component';
+import {SkillsComponent} from './main/skills/skills.component';
+import {ContactComponent} from './main/contact/contact.component';
+import {ProjectsComponent} from './main/projects/projects.component';
+import {BlogComponent} from './main/blog/blog.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'blog', component: BlogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { ProjectsComponent } from './main/projects/projects.component';
+import { BlogComponent } from './main/blog/blog.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { HeaderComponent } from './common/header/header.component';
+import { SidenavListComponent } from './common/sidenav-list/sidenav-list.component';
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ContactComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
